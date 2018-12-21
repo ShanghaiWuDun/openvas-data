3c4926ed59371f962c5c5dbba7492cbe7cec5255
luozijun <luozijun.assistant@gmail.com>
2018-12-21 19:15:29 +0800

    add cert-data.tar.bz2 nvt-feed.tar.bz2 scap-data.tar.bz2

--- rsync.sh
+++ rsync.sh
@@ -3,3 +3,7 @@
 rsync -avzH --delete rsync://feed.openvas.org:/nvt-feed  nvt-feed
 rsync -avzH --delete rsync://feed.openvas.org:/scap-data scap-data
 rsync -avzH --delete rsync://feed.openvas.org:/cert-data cert-data
+
+# tar -cvJf nvt-feed.tar.bz2 nvt-feed
+# tar -cvJf cert-data.tar.bz2 cert-data
+# tar -cvJf scap-data.tar.bz2 scap-data
